3e38bfe26458dc7afc21685b6b0434e9a4ea52e8
adeeba reaz <adeebarivera@outlook.com>
2020-04-24 15:14:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,19 @@
 
-def roll_call_dwarves ; ["Doc", "Dopey", "Bashful", "Grumpy", "sneezy", "sleepy", "happy"]
-end
-print roll_call_dwarves
+# def roll_call_dwarves ; ["Doc", "Dopey", "Bashful", "Grumpy", "sneezy", "sleepy", "happy"]
+# end
+# print roll_call_dwarves
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
 
